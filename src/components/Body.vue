865c8a7f476334745efb33d36e85e052ece90803
Alex Peebles <alex_peebles@outlook.com>
2024-11-24 16:50:06 +0000

    fix: remove rsvp

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -10,6 +10,5 @@
     <About/>
     <Venue/>
     <Schedule/>
-    <RSVP/>
 </template>
   
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './index.css'
+import MainView from './views/Main.vue'
 import App from './App.vue'
+import LoginView from './views/Login.vue'
 import { createMemoryHistory, createRouter } from 'vue-router'
 
-import AppView from './App.vue'
-
 const routes = [
-    { path: '/', component: AppView },
+    { path: '/',
+      name: 'main',
+      component: MainView,
+      meta: {
+        requiresAuth: true
+     }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    }
   ]
 
 const router = createRouter({
@@ -15,9 +26,24 @@ history: createMemoryHistory(),
 routes,
 })
 
-const pinia = createPinia()
-const app = createApp(App)
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const token = localStorage.getItem('token');
+    if (token) {
+      // User is authenticated, proceed to the route
+      next();
+    } else {
+      // User is not authenticated, redirect to login
+      next('/login');
+    }
+  } else {
+    // Non-protected route, allow access
+    next();
+  }
+});
 
-app.use(pinia)
-app.use(router)
-app.mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+app.mount('#app');
